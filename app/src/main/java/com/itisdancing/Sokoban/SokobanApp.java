1b1fdad80e2b4aa269801935bdd198d5bee70d00
David <s1098797@ouhk.edu.hk>
2015-11-14 19:13:36 +0800

    added settings button and about

--- app/src/main/java/com/itisdancing/Sokoban/SokobanApp.java
+++ app/src/main/java/com/itisdancing/Sokoban/SokobanApp.java
@@ -44,7 +44,7 @@ public class SokobanApp extends Activity implements OnClickListener
           startActivity(scores);
         case R.id.settings_button:
           //write PreferenceActivity codes
-        case R.id.about_button:
+        case R.id.about_us_button:
             new AlertDialog.Builder(this).setTitle(R.string.about).setMessage("Member:xxx").setNeutralButton("OK", null).show();
       }
     }
